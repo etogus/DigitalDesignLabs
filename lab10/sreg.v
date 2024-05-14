25d9628da9f8ef9286b6fcf58d1e7ee13abdb5c6
etogus <47570845+etogus@users.noreply.github.com>
2024-05-14 18:33:45 +0200

    Add files via upload

---
+++ lab10/sreg.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+
+module sreg(
+    sta,
+    act,
+    inp,
+    nxt,
+    ck,
+    rn,
+    out
+);
+
+input [0:63] inp; // State initial data
+input [0:63] nxt; // Next value of state
+input act;        // Active signal
+input sta;        // Start signal
+input ck;         // Rising edge clock
+input rn;         // Active low reset
+output [0:63] out; // State output
+reg [0:63] out; // State output
+
+always @ (posedge ck or negedge rn)
+    if (!rn)
+        out <= 0;
+    else if (sta)
+        out <= inp;
+    else if (act)
+        out <= nxt;
+
+endmodule
